70efc02b572beb96a44762bde79277bf5f578dc0
gazala-hafsa7 <gazalahafsa12@gmail.com>
2026-05-18 15:45:21 +0530

    fix: resolve CI issues

--- lib/svg/themes.ts
+++ lib/svg/themes.ts
@@ -57,10 +57,10 @@ export const themes: Record<string, BadgeTheme> = {
     text: 'f8f8f2',
     accent: 'ff2d78',
   },
-  gruvbox:{
-    bg:'282828',
-    text:'ebdbb2',
-    accent:'fe8019',
+  gruvbox: {
+    bg: '282828',
+    text: 'ebdbb2',
+    accent: 'fe8019',
   },
 };
 
