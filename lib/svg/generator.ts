3d8b2e20bee054cd80aeffe42f5e289e7ad7c9ab
Sahil Shrivastva <shrivastvasahil78@gmail.com>
2026-05-28 10:30:10 +0530

    fix: import FONT_MAP from constants

--- lib/svg/generator.ts
+++ lib/svg/generator.ts
@@ -4,7 +4,7 @@ import { AUTO_THEME_DARK, AUTO_THEME_LIGHT } from './themes';
 import { TOWER_ANIMATION_CSS } from './animations';
 import { computeTowers, type TowerData } from './layout';
 import { sanitizeFont, sanitizeHexColor, sanitizeRadius, sanitizeGoogleFontUrl } from './sanitizer';
-import { SVG_WIDTH, SVG_HEIGHT } from './constants';
+import { SVG_WIDTH, SVG_HEIGHT, FONT_MAP } from './constants';
 
 // helpers
 function truncateUsername(name: string, max = 20): string {
