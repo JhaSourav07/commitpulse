272b0dfbf8220ce10d893e19f8e9a4fe7c46cabf
pisum-sativum <ankitajha102@gmail.com>
2026-07-24 13:06:09 +0530

    style: reformat files to match origin/main prettier config

--- components/burnout/BurnoutRiskTable.tsx
+++ components/burnout/BurnoutRiskTable.tsx
@@ -1,7 +1,7 @@
 'use client';
 
-import Image from 'next/image';
 import { useState, useMemo } from 'react';
+import Image from 'next/image';
 import { motion } from 'framer-motion';
 import {
   Flame,
